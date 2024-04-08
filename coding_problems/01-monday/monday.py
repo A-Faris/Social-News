e6d705fcc2cba294c23bd09b651579e1500799a9
Faris Abulula <abululb02@gmail.com>
2024-04-08 14:22:02 +0100

    coding problem done

--- coding_problems/01-monday/monday.py
+++ coding_problems/01-monday/monday.py
@@ -1,2 +1,10 @@
 def kebabize(st: str) -> str:
-    pass
+    kebab = ""
+    for letter in st:
+        if letter.isalpha():
+            if letter.isupper():
+                kebab += "-"
+            kebab += letter.lower()
+    if kebab and kebab[0] == "-":
+        kebab = kebab[1:]
+    return kebab
